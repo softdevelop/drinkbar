984ba7a712601de0ba26868b89d4b46c105c0662
softdevelophd <softdevelop.hd@gmail.com>
2018-04-09 08:59:50 +0700

    updated scss drink image

--- blur-admin/src/app/pages/manager-ingredient/import/import-ctrl.js
+++ blur-admin/src/app/pages/manager-ingredient/import/import-ctrl.js
@@ -17,17 +17,56 @@
         }
 
         // ============ get list ingredient ==========
-        function getListIngredient(){
-            var offset = 0;
-            IngredientService.getList($rootScope.userLogin.token, offset).success(function(res){
-                $scope.ingredients = res.results;
-            }).error(function(err, stt, res){
-                console.log(res)
-                toastr.error('Error!')
+        // function getListIngredient(){
+        //     var offset = 0;
+        //     IngredientService.getList($rootScope.userLogin.token, offset).success(function(res){
+        //         $scope.ingredients = res.results;
+        //     }).error(function(err, stt, res){
+        //         console.log(res)
+        //         toastr.error('Error!')
+        //     })
+        // }
+
+        // getListIngredient();
+
+         // =========== get list ingredient ==============
+         function getListIngredient(type, brand) {
+            DrinkService.getListIngredient($rootScope.userLogin.token, type, brand).success(function (res) {
+                $scope.list_ingredient = res.results;
             })
         }
 
-        getListIngredient();
+        // getListIngredient();
+
+        // ========== function get list type ============
+        function getListType() {
+            $scope.types = [];
+            $scope.list_ingredient = [];
+            $rootScope.brands = [];
+            IngredientService.getListType($rootScope.userLogin.token).success(function (res) {
+                $scope.types = res;
+                getListBrand(res.id);
+                $scope.types.length === 0 && ($scope.isAddElement.type = true);
+            }).error(function (err, stt, res) {
+                console.log(res)
+                toastr.error('Error!');
+            });
+        }
+
+        getListType();
+
+        // ============ function get list brand ===========
+        function getListBrand(type) {
+            $scope.list_ingredient = [];
+            $rootScope.brands = [];
+            DrinkService.getListBrand($rootScope.userLogin.token, type).success(function (res) {
+                $rootScope.brands = res;
+                $rootScope.brands.length === 0 && ($scope.isAddElement.brand = true);
+            }).error(function (err, stt, res) {
+                console.log(res)
+                toastr.error('Error!');
+            });
+        }
 
         // ========== function change from ===============
 		$scope.changeInfo = function(field, value){
